c0b76964cc2f3d59afd2bf59d3e9a4f4e5deb85b
Vladimir8606 <cisco8606@mail.ru>
2023-05-09 13:28:09 +0200

    Add project files.

---
+++ Check_even_odd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Check_even_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value: ");
+            string str = Console.ReadLine();
+            int value = int.Parse(str);
+            if (value%2 == 0)
+                Console.WriteLine("Value is even");
+            else
+                Console.WriteLine("Value is odd");
+        }
+    }
+}
---
+++ test2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            string str, str2;
+            double rate = 18.30;
+            Console.WriteLine("Enter carrency you want to change USD or ZAR: ");
+            str = Console.ReadLine();
+            Console.WriteLine("Enter amount: ");
+            str2 = Console.ReadLine();
+            b=Convert.ToInt32(str2);
+            if (str == "USD") 
+            {
+                double result = b * rate;
+                Console.WriteLine("You'll recive " + result + "ZAR");
+            }
+            else if (str == "ZAR")
+            {
+                double result = b/rate;
+                Console.WriteLine("You'll recive " + result + "USD");
+
+            }
+            else
+            {
+                Console.WriteLine("Incorrect carrency");
+            }
+
+        }
+    }
+}
