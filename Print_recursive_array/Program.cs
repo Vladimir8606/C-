9fe0b1db2f2d4a3ddac348745e7fee38fb01e0c2
Vladimir8606 <cisco8606@mail.ru>
2023-05-23 09:19:53 +0200

    123

---
+++ Print_recursive_array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_recursive_array
+{
+    internal class Program
+    {
+        static void Print(int[] arr,int value)
+        {
+            
+            if (value >= arr.Length)
+            {
+                return;
+            }
+            Console.WriteLine(arr[value]);
+            value++;
+            Print(arr,value);
+        }
+
+        static void SUM(int[] arr, int value, int summ)
+        {
+            if (value >= arr.Length)
+            {
+                Console.WriteLine("Summary is : " + summ);
+                return;
+            }
+            summ += arr[value];
+            value++;
+            SUM(arr, value, summ);
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 5, 10, 6, -3, 45 };
+            int i = 0;
+            int summ = 0;
+            Print(myArray, i);
+
+            Console.WriteLine();
+            SUM(myArray, i,summ);
+        }
+    }
+}
--- test2/Program.cs
+++ test2/Program.cs
@@ -5,36 +5,17 @@ namespace test2
 {
     internal class Program
     {
+        static void SUN(int a)
+        {
+            Console.WriteLine(a);
+
+            a++;
+            SUN(a);
+        }
+
         static void Main(string[] args)
         {
-            //int[] myArray = { 10, 2, 3, 5, 10, 66, 34, 23, 3, 2 };
-            //  int[] result = myArray.Distinct().ToArray();
-            // for(int i = 0; i < result.Length; i++)
-            //    Console.WriteLine(result[i]);
-
-            int[,] myArray2 = new int[10, 10];
-
-            Random random = new Random();
-
-            for (int i = 0; i < myArray2.GetLength(0); i++)
-            {
-                for (int j = 0; j < myArray2.GetLength(1); j++)
-                {
-                    myArray2[i, j] = random.Next(100);
-                }
-            }
-
-
-
-            for (int i = 0; i < myArray2.GetLength(0); i++)
-            {
-                for (int j = 0; j < myArray2.GetLength(1); j++)
-                {
-                    Console.Write(myArray2[i, j] + "\t");
-
-                }
-                Console.WriteLine();
-            }
+            SUN(0);
                 
         }
     }
